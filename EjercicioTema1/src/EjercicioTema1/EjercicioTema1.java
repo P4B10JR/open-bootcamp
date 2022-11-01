18fa17e6fe80fc3562886f6914f4ec335f8f13cb
Pablo Jose Rosales <86581920+P4B10JR@users.noreply.github.com>
2022-11-01 12:51:24 -0300

    Add files via upload

---
+++ EjercicioTema1/src/EjercicioTema1/EjercicioTema1.java
@@ -0,0 +1,25 @@
+package EjercicioTema1;
+
+public class EjercicioTema1 {
+
+    public static void main(String[] args) {
+
+
+        int var0 = 5;
+
+        long var1 = 500;
+
+        double var2 = 50.0;
+
+        boolean var3 = true;
+
+        String var4 = "false";
+
+        System.out.println(var0);
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+    }
+
+}
